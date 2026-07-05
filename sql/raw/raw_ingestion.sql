9952abedc21397c8ca447574b96b1107c58a572c
Muhammad Adnan <your_email@example.com>
2026-07-05 10:10:45 +0500

    Added inferSchema to know the layout of table

--- sql/raw/raw_ingestion.sql
+++ sql/raw/raw_ingestion.sql
@@ -17,3 +17,16 @@ CREATE OR REPLACE FILE FORMAT infer_schema_csv
 TYPE = CSV 
 PARSE_HEADER = TRUE 
 FIELD_OPTIONALLY_ENCLOSED_BY='"';
+
+
+--VALIDATING FILE_FORMAT
+
+SELECT *
+FROM TABLE(
+            infer_schema(
+                LOCATION=>'@ecom_stage',
+                FILE_FORMAT=>'infer_schema_csv',
+                FILES=>('olist_customers_dataset.csv.gz')
+            )
+);
+
