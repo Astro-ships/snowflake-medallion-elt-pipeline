a5eba42bf522fadbd6aef48ffd1b470d88a7c8ea
Muhammad Adnan <your_email@example.com>
2026-07-05 20:19:42 +0500

    Create table 'bronze.orders From raw.orders

--- sql/bronze/bronze.orders.sql
+++ sql/bronze/bronze.orders.sql
@@ -5,3 +5,18 @@ USE ROLE ACCOUNTADMIN;
 USE WAREHOUSE compute_wh;
 USE DATABASE ecommerce_db;
 USE SCHEMA BRONZE;
+-- ==========================================================
+-- CREATE CUSTOMER TABLE FROM RAW DATA
+-- ==========================================================
+
+CREATE OR REPLACE TABLE bronze.orders AS 
+SELECT 
+        "order_id",
+        "customer_id",
+        "order_purchase_timestamp",
+        "order_approved_at",
+        "order_delivered_carrier_date",
+        "order_delivered_customer_date",
+        "order_estimated_delivery_date"
+
+FROM RAW.ORDERS
