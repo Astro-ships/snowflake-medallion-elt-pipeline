6280995399a2323130e0b727f86ffec6d61cfa28
Muhammad Adnan <your_email@example.com>
2026-07-05 10:16:59 +0500

    Extract the blueprint of table to let snowflake create the table for us.

--- sql/raw/raw_ingestion.sql
+++ sql/raw/raw_ingestion.sql
@@ -30,3 +30,17 @@ FROM TABLE(
             )
 );
 
+--Extract the blueprint of table to let snowflake create the table for us.
+--1 Customer Table;
+CREATE OR REPLACE TABLE RAW.CUSTOMERS 
+USING TEMPLATE(
+                SELECT ARRAY_AGG(OBJECT_CONSTRUCT(*))
+                FROM TABLE(
+                            infer_schema(
+                                            LOCATION=>'@ecom_stage',
+                                            FILE_FORMAT=>'INFER_SCHEMA_CSV',
+                                            FILES=>('olist_customers_dataset.csv.gz')
+                            )
+                )
+);
+
