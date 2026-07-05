4b351e431f5deb0d90e931fad5a79093f6a9abbd
Muhammad Adnan <your_email@example.com>
2026-07-05 09:22:00 +0500

    Validated files in stage

--- sql/raw/raw_ingestion.sql
+++ sql/raw/raw_ingestion.sql
@@ -3,3 +3,9 @@ USE ROLE ACCOUNTADMIN;
 USE WAREHOUSE compute_wh;
 USE DATABASE ecommerce_db;
 USE SCHEMA raw;
+ 
+ --Verifying stage 
+ SHOW STAGES;
+
+-- Stage confirmed, Validating data
+ls@ecom_stage;
