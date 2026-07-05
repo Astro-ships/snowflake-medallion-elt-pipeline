dd6b971416a554e78472ab6b625b36c8b3942ace
Muhammad Adnan <your_email@example.com>
2026-07-05 18:44:56 +0500

    Create Bronze geolocation table

--- sql/bronze/bronze.geolocation.sql
+++ sql/bronze/bronze.geolocation.sql
@@ -5,3 +5,16 @@ USE ROLE ACCOUNTADMIN;
 USE WAREHOUSE compute_wh;
 USE DATABASE ecommerce_db;
 USE SCHEMA BRONZE;
+
+-- ==========================================================
+-- CREATE CUSTOMER TABLE FROM RAW DATA
+-- ==========================================================
+
+CREATE OR REPLACE TABLE bronze.geolocation AS
+SELECT 
+        "geolocation_zip_code_prefix", 
+        "geolocation_lat",
+        "geolocation_lng",
+        UPPER("geolocation_city") as geolocation_city,
+        "geolocation_state"
+FROM RAW.GEOLOCATION;
