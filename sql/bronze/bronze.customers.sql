3cc0c1a1eaf5302f0855c9965327b8959f835b70
Muhammad Adnan <your_email@example.com>
2026-07-05 18:14:27 +0500

    Configure Snowflake Session

---
+++ sql/bronze/bronze.customers.sql
@@ -0,0 +1,8 @@
+-- ==========================================================
+-- Configure Snowflake session
+-- ==========================================================
+USE ROLE ACCOUNTADMIN;
+USE WAREHOUSE compute_wh;
+USE DATABASE ecommerce_db;
+USE SCHEMA BRONZE;
+
