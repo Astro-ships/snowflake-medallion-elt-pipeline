15ab5bdac9b8935f0feacec90fd8bef8a2a25b36
Muhammad Adnan <your_email@example.com>
2026-07-06 12:20:16 +0500

    Created bronze product_category table

--- sql/bronze/bronze.product_category.sql
+++ sql/bronze/bronze.product_category.sql
@@ -5,3 +5,11 @@ USE ROLE ACCOUNTADMIN;
 USE WAREHOUSE compute_wh;
 USE DATABASE ecommerce_db;
 USE SCHEMA BRONZE;
+-- ==========================================================
+-- CREATE PRODUCT_CATEGORY TABLE FROM RAW DATA
+-- ==========================================================
+CREATE OR REPLACE TABLE BRONZE.PRODUCT_CATEGORY AS 
+SELECT 
+        "product_category_name" AS product_category_name,
+         "product_category_name_english" AS product_category_name_english
+FROM RAW.PRODUCT_CATEGORY;
