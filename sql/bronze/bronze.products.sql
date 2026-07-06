82ca721faef92a48bef5206848edae7fc52a5ddb
Muhammad Adnan <your_email@example.com>
2026-07-06 11:49:25 +0500

    Created bronze products table

--- sql/bronze/bronze.products.sql
+++ sql/bronze/bronze.products.sql
@@ -5,3 +5,18 @@ USE ROLE ACCOUNTADMIN;
 USE WAREHOUSE compute_wh;
 USE DATABASE ecommerce_db;
 USE SCHEMA BRONZE;
+-- ==========================================================
+-- CREATE PRODUCTS TABLE FROM RAW DATA
+-- ==========================================================
+CREATE OR REPLACE TABLE BRONZE.products AS 
+SELECT 
+        "product_id" AS product_id,
+        "product_category_name" AS product_category_name,
+        "product_name_lenght" AS product_name_lenght,
+        "product_description_lenght" AS product_description_length,
+        "product_photos_qty" AS product_photos_qty,
+        "product_weight_g" AS product_weight_g,
+        "product_length_cm" AS product_length_cm,
+        "product_height_cm" AS product_height_cm,
+        "product_width_cm" AS product_width_cm 
+FROM RAW.products
