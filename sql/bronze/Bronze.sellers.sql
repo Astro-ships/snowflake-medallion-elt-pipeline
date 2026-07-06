4ca9717ae1db0551169c7d402fc82536fd289e87
Muhammad Adnan <your_email@example.com>
2026-07-06 12:45:31 +0500

    created bronze.sellers table from raw table

--- sql/bronze/Bronze.sellers.sql
+++ sql/bronze/Bronze.sellers.sql
@@ -5,3 +5,14 @@ USE ROLE ACCOUNTADMIN;
 USE WAREHOUSE compute_wh;
 USE DATABASE ecommerce_db;
 USE SCHEMA BRONZE;
+
+-- ==========================================================
+-- CREATE SELLERS TABLE FROM RAW DATA
+-- ==========================================================
+CREATE OR REPLACE TABLE BRONZE.SELLERS AS 
+SELECT 
+        "seller_id" AS seller_id,
+        "seller_zip_code_prefix" AS seller_zip_code_prefix,
+        "seller_city" AS  seller_city,
+        "seller_state" AS seller_state 
+FROM RAW.SELLERS
