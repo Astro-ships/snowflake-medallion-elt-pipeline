c337912a28151550a19113b37fb1dd7c6aaf4f7e
Muhammad Adnan <your_email@example.com>
2026-07-06 08:58:04 +0500

    Refine Bronze orders table schema

--- sql/bronze/bronze.orders.sql
+++ sql/bronze/bronze.orders.sql
@@ -11,13 +11,14 @@ USE SCHEMA BRONZE;
 
 CREATE OR REPLACE TABLE bronze.orders AS 
 SELECT 
-        "order_id",
-        "customer_id",
-        "order_purchase_timestamp",
-        "order_approved_at",
-        "order_delivered_carrier_date",
-        "order_delivered_customer_date",
-        "order_estimated_delivery_date"
+        "order_id" AS order_id,
+        "customer_id" AS customer_id,
+        "order_status" AS order_status,
+        "order_purchase_timestamp" AS order_purchase_timestamp,
+        "order_approved_at" AS order_approved_at,
+        "order_delivered_carrier_date" AS order_delivered_carrier_date,
+        "order_delivered_customer_date" AS order_delivered_customer_date,
+        "order_estimated_delivery_date" AS order_estimated_delivery_date
 
 FROM RAW.ORDERS
 
