6d566ecf5849fdba071d9b3600da34b68b099a91
Muhammad Adnan <your_email@example.com>
2026-07-06 10:48:45 +0500

    Created bronze table of order_items

--- sql/bronze/bronze.order_items.sql
+++ sql/bronze/bronze.order_items.sql
@@ -5,3 +5,19 @@ USE ROLE ACCOUNTADMIN;
 USE WAREHOUSE compute_wh;
 USE DATABASE ecommerce_db;
 USE SCHEMA BRONZE;
+
+-- ==========================================================
+-- CREATE ORDER_ITEMS TABLE FROM RAW DATA
+-- ==========================================================
+
+CREATE OR REPLACE TABLE BRONZE.ORDER_ITEMS AS 
+SELECT 
+    "order_id" AS order_id,
+    "order_item_id" AS order_item_id,
+    "product_id" AS product_id,   
+    "seller_id" AS seller,
+    "shipping_limit_date" AS shipping_limit_date,
+    "price" AS price,
+    "freight_value" AS freight_value
+FROM RAW.ORDER_ITEMS
+
