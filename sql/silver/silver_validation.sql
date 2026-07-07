b279cba2e79e592e30c8b0978816edcb36888bf0
Muhammad Adnan <your_email@example.com>
2026-07-07 11:08:22 +0500

    Configure Snowflake session

---
+++ sql/silver/silver_validation.sql
@@ -0,0 +1,9 @@
+-- ==========================================================
+-- Configure Snowflake session
+-- ==========================================================
+USE ROLE ACCOUNTADMIN;
+USE WAREHOUSE compute_wh;
+USE DATABASE ecommerce_db;
+USE SCHEMA SILVER;
+ 
+
