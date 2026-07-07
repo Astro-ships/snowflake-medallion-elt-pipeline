ce4737d912050f0cf05ffcc39e8db558f20288a4
Muhammad Adnan <your_email@example.com>
2026-07-07 14:26:51 +0500

    Data transformation and Standardized Silver layer table created

--- sql/silver/silver.customers.sql
+++ sql/silver/silver.customers.sql
@@ -9,6 +9,7 @@ USE SCHEMA SILVER;
 -- ==========================================================
 -- Data Profiling
 -- ==========================================================
+
 -- Check for columns 
 SHOW COLUMNS IN TABLE BRONZE.CUSTOMERS;
 -- Check customer_id uniqueness
@@ -68,3 +69,35 @@ SELECT
         COUNT(*) 
 FROM BRONZE.CUSTOMERS
 WHERE customer_State IS NULL;
+
+-- ==========================================================
+-- Data Transformation
+-- ==========================================================
+
+-- Fix ZIP code prefix length
+SELECT
+    customer_zip_code_prefix AS original_zip_prefix,
+    LPAD(customer_zip_code_prefix, 5, '0') AS standardized_zip_prefix
+FROM bronze.customers
+LIMIT 50;
+
+--standardize customer_city 
+SELECT 
+        customer_city AS original_city,
+        LOWER(customer_city) AS standardized_city
+FROM BRONZE.CUSTOMERS
+LIMIT 20;
+
+-- ==========================================================
+-- CREATE STANDARDIZED SILVER LAYER CUSTOMER TABLE
+-- ==========================================================
+CREATE OR REPLACE TABLE SILVER.CUSTOMERS
+AS
+SELECT
+        customer_id,
+        customer_unique_id,
+        LPAD(CUSTOMER_ZIP_CODE_PREFIX,5,'0') as customer_zip_code_prefix,
+        LOWER(customer_city) AS customer_city,
+        customer_state
+FROM BRONZE.CUSTOMERS
+
