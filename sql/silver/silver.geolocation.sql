6fb5d51f8f6523aecb1525c532c641408c9f2127
Muhammad Adnan <your_email@example.com>
2026-07-08 10:24:53 +0500

    Data Profiling Bronze.geolocation

---
+++ sql/silver/silver.geolocation.sql
@@ -0,0 +1,121 @@
+-- ==========================================================
+-- Configure Snowflake session
+-- ==========================================================
+USE ROLE ACCOUNTADMIN;
+USE WAREHOUSE compute_wh;
+USE DATABASE ecommerce_db;
+USE SCHEMA SILVER;
+-- ==========================================================
+-- Data Profiling
+-- ==========================================================
+
+-- Check for columns 
+SHOW COLUMNS IN TABLE BRONZE.geolocation;
+
+-- Check for column values 
+DESCRIBE TABLE BRONZE.GEOLOCATION;
+--======================================
+-- Check customer_zip_code_prefix values
+--======================================
+---- Check for NULL values
+SELECT 
+        COUNT(*)
+FROM BRONZE.GEOLOCATION
+WHERE GEOLOCATION_ZIP_CODE_PREFIX IS NULL;
+
+--No Nulls Found
+-- Check ZIP code length
+SELECT
+        COUNT(*) as total_rows,
+        LENGTH(GEOLOCATION_ZIP_CODE_PREFIX) AS prefix_length
+FROM BRONZE.GEOLOCATION
+GROUP BY prefix_length
+ORDER BY prefix_length;
+-- Result:
+-- Some ZIP code prefixes contain fewer than 5 digits.
+-- Investigation:
+-- ZIP code prefixes are stored as NUMBER, so leading zeros are removed.
+-- ZIP codes should be standardized to a fixed 5-character format.
+SELECT 
+        LPAD(GEOLOCATION_ZIP_CODE_PREFIX,5,'0') AS GEOLOCATION_ZIP_CODE_PREFIX
+FROM BRONZE.GEOLOCATION
+LIMIT 50;
+--Values standardized to 5 prefix
+--======================================
+-- Check GEOLOCATION_LAT Values
+--======================================
+--Check for nulls
+SELECT 
+        COUNT(*)
+FROM BRONZE.geolocation
+WHERE geolocation_lat IS NULL;
+
+-- Result: No Nulls found
+-- Validate latitude values are within the valid geographic range (-90 to 90).
+SELECT 
+     GEOLOCATION_CITY
+FROM BRONZE.GEOLOCATION
+WHERE GEOLOCATION_LAT > 90 OR GEOLOCATION_LAT < -90;
+-- Result:No NULLS found
+-- All latitude values are within the valid geographic range (-90 to 90).
+--======================================
+-- Check GEOLOCATION_LNG  Values
+--======================================
+-- Check for nulls.
+SELECT 
+        COUNT(*)
+FROM BRONZE.GEOLOCATION
+WHERE GEOLOCATION_LNG IS NULL;
+--Result: No Nulls found
+-- Validate longtitude values are within the valid geographic range (-180 to 180)./
+
+SELECT 
+        GEOLOCATION_CITY
+FROM BRONZE.GEOLOCATION
+WHERE GEOLOCATION_LNG > 180 OR GEOLOCATION_LNG < -180;
+
+-- Result:
+-- All longitude values are within the valid geographic range (-180 to 180).
+
+--======================================
+-- Check GEOLOCATION_CITY 
+--======================================
+--Check for nulls
+SELECT 
+        COUNT(*)
+FROM BRONZE.GEOLOCATION
+WHERE GEOLOCATION_CITY IS NULL;
+
+-- Result: No Nulls found
+-- Inspect GEOLOCATION_CITY for consistency
+SELECT 
+      DISTINCT  GEOLOCATION_CITY
+FROM BRONZE.GEOLOCATION
+-- Result:
+-- Multiple formatting inconsistencies were found, including
+-- differences in capitalization and accented characters.
+-- City names will be standardized in the Silver layer.
+SELECT 
+     DISTINCT(TRANSLATE(INITCAP(LOWER(geolocation_city)),'áàâãéêíóôõúç',
+    'aaaaeeiooouc') ) as geolocation_city
+FROM bronze.geolocation
+
+-- Evaluate city name standardization.
+-- Convert values to title case and remove accented characters
+-- to verify the expected standardized output.
+
+--======================================
+-- Check GEOLOCATION_STATE
+--======================================
+
+-- Check for NULL
+SELECT 
+        COUNT(*)
+FROM BRONZE.GEOLOCATION
+WHERE GEOLOCATION_STATE IS NULL;
+--Result: No NULL rows
+-- Inspect GEOLOCATION_CITY for consistency
+SELECT
+      DISTINCT  GEOLOCATION_STATE
+FROM BRONZE.GEOLOCATION
+--Result: Data is consistent
