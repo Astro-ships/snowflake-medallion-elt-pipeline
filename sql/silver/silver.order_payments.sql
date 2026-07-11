3d59945d7e68683a1168fbda849e8ec2b6cdf5d0
Muhammad Adnan <your_email@example.com>
2026-07-11 16:56:31 +0500

    Standardized Comments for better readability

--- sql/silver/silver.order_payments.sql
+++ sql/silver/silver.order_payments.sql
@@ -8,36 +8,40 @@ USE SCHEMA SILVER;
 -- ==========================================================
 -- Data Profiling
 -- ==========================================================
-
+---------------------
 -- Check for columns 
+---------------------
 SHOW COLUMNS IN TABLE BRONZE.ORDER_PAYMENTS;
 --======================================
 -- Check ORDER_ID / PAYMENT_SEQUENTIAL
 --======================================
-
--- Check PAYMENT_SEQUENTIAL uniqueness
+------------------------------------------
+-- Check PAYMENT_SEQUENTIAL uniqueness.
+------------------------------------------
 SELECT
     COUNT(*) AS total_rows,
     COUNT(DISTINCT payment_sequential) AS unique_payment_sequential
 FROM BRONZE.ORDER_PAYMENTS;
-
+---------------------------------------------------------------
 -- Result:
 -- Duplicate values exist because PAYMENT_SEQUENTIAL
 -- restarts for each order.
-
--- Check ORDER_ID uniqueness
+---------------------------------------------------------------
+-- Check ORDER_ID uniqueness.
+----------------------------
 SELECT
     COUNT(*) AS total_rows,
     COUNT(DISTINCT order_id) AS unique_order_id
 FROM BRONZE.ORDER_PAYMENTS;
-
+---------------------------------------------------------------
 -- Result:
 -- Duplicate values exist because an order can contain
 -- multiple payment records.
-
+---------------------------------------------------------------
 -- Investigation:
 -- Validate the composite key (ORDER_ID, PAYMENT_SEQUENTIAL),
 -- which uniquely identifies each payment record.
+---------------------------------------------------------------
 WITH unique_payments AS (
     SELECT DISTINCT
         order_id,
@@ -48,29 +52,36 @@ SELECT
     (SELECT COUNT(*) FROM BRONZE.ORDER_PAYMENTS) AS total_rows,
     COUNT(*) AS unique_combinations
 FROM unique_payments;
-
+---------------------------------------------------------------
 -- Result:
 -- All (ORDER_ID, PAYMENT_SEQUENTIAL) combinations are unique.
-
+---------------------------------------------------------------
 --======================================
 -- Check PAYMENT_TYPE
 --======================================
 --Check for NULLS
+------------------------------------------
 SELECT 
         COUNT(*)
 FROM BRONZE.ORDER_PAYMENTS
 WHERE payment_type IS NULL;
---Result:None 
---Inspect for values
+------------------------------------------
+-- Result:None 
+-- Inspect for values
+------------------------------------------
 SELECT DISTINCT 
                 payment_type
 FROM BRONZE.ORDER_PAYMENTS;
+--------------------------------------------
 --Result: Data looks standardized and valid.
+------------------------------------------
 
 --======================================
 -- PAYMENT_INSTALLMENTS
 --======================================
---Check for NULLS
+---------------------
+-- Check for NULLS.
+---------------------
 SELECT 
         COUNT(*)
 FROM BRONZE.ORDER_PAYMENTS
@@ -79,14 +90,18 @@ WHERE payment_installments IS NULL;
 --======================================
 -- PAYMENT_VALUE
 --======================================
---Check for Nulls
+---------------------
+-- Check for Nulls.
+---------------------
 
 SELECT 
         COUNT(*)
 FROM BRONZE.ORDER_PAYMENTS
 WHERE PAYMENT_VALUE IS NULL;
---Result:None 
---Check for negative payment value
+------------------------------------------
+-- Result:None. 
+-- Check for negative payment value
+------------------------------------------
 SELECT *
 FROM BRONZE.ORDER_PAYMENTS
 WHERE PAYMENT_VALUE < 0;
@@ -97,6 +112,7 @@ WHERE PAYMENT_VALUE < 0;
 -- No transformations required.
 -- Data quality validation confirmed that the table contains
 -- consistent values and valid business relationships.
+---------------------------------------------------------------
 CREATE OR REPLACE TABLE SILVER.ORDER_PAYMENTS
 AS
 SELECT * 
